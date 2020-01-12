bc9b357545ce2c4afbec53fb888f586ebaed08ee
aakash <aakashgupta10@yahoo.com>
2020-01-12 22:40:31 +0530

    Updated URL

--- src/main/java/base/Constants.java
+++ src/main/java/base/Constants.java
@@ -13,9 +13,7 @@ public class Constants {
 	public static final String TEST_DATA_PROP_FILE_PATH = "src/main/resources/properties/testdata.properties";
 
 	public static final String BROWSER = "chrome";
-	public static final String URL = "https://app.vwo.com/#/analyze/heatmap/129/reports?token=eyJhY2NvdW50X2lkIjo2LCJleHBlc\n" +
-			"mltZW50X2lkIjoxMjksImNyZWF0ZWRfb24iOjE1MDc3ODk0ODcsInR5cGUiOiJjYW1wYWlnbiIsI\n" +
-			"nZlcnNpb24iOjEsImhhc2giOiJiMzlmMTQ4MWE0ZDMyN2Q4MDllNTM1YzVlNWFjOGVlMCJ9";
+	public static final String URL = "www.google.com";
 
 	
 	public static final long DRIVER_TIME_OUT = 15;
